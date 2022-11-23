7da1eea3fdaac4b387b44a3abd97588717676d06
Hakan Bilgi <hknblg34@gmail.com>
2022-11-23 18:03:12 +0300

    ...

--- src/hooks/useStockCalls.jsx
+++ src/hooks/useStockCalls.jsx
@@ -5,18 +5,33 @@ import { fetchFail, fetchStart, getSuccess } from "../features/stockSlice";
 const useStockCalls = () => {
   const dispatch = useDispatch();
 
-  const getFirms = async () => {
-    const url = "firms";
+  // const getFirms = async () => {
+  //   const url = "firms";
+  //   dispatch(fetchStart());
+  //   try {
+  //     const { data } = await axiosWithToken.get(`stock/firms`);
+  //     console.log(data);
+  //     dispatch(getSuccess({ data, url }));
+  //   } catch (error) {
+  //     dispatch(fetchFail());
+  //     console.log(error);
+  //   }
+  // };
+  const getStockData = async (url) => {
     dispatch(fetchStart());
     try {
-      const { data } = await axiosWithToken.get(`stock/firms`);
-      console.log(data);
+      const { data } = await axiosWithToken.get(`stock/${url}`);
       dispatch(getSuccess({ data, url }));
     } catch (error) {
       dispatch(fetchFail());
       console.log(error);
     }
   };
+
+  const getFirms = () => getStockData("firms");
+  const getSales = () => getStockData("sales");
+  
+  
 
   return { getFirms };
 };
